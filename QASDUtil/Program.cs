cd9e0135fee3303ada43a10ba56be09ae9e108a2
Doug Webster <doug.k.webster@gmail.com>
2020-04-11 15:17:16 -0700

    Added pigpio client

---
+++ QASDUtil/Program.cs
@@ -0,0 +1,15 @@
+﻿using QuadActuatorStandupDesk;
+using System;
+
+namespace QASDUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quad Actuator Standup Desk Util!");
+
+            Class1.Testing();
+        }
+    }
+}
---
+++ QuadActuatorStandupDesk/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Rapidnack.Net;
+using System;
+using System.Threading;
+
+namespace QuadActuatorStandupDesk
+{
+    public class Class1
+    {
+        public static void Testing()
+        {
+            var pigpiodIf = new PigpiodIf();
+            pigpiodIf.pigpio_start("192.168.1.10", "8888");
+            const int GPIO = 5;
+
+            while (true)
+            {
+                pigpiodIf.gpio_write(GPIO, PigpiodIf.PI_HIGH);
+                Thread.Sleep(500);
+                pigpiodIf.gpio_write(GPIO, PigpiodIf.PI_LOW);
+                Thread.Sleep(500);
+            }
+
+        }
+    }
+}
