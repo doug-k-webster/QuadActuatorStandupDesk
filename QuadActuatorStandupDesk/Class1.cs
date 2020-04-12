0ae4bf7e60a8099e521331dfc6f01d3af5a2d932
Doug Webster <doug.k.webster@gmail.com>
2020-04-12 15:08:32 -0700

    sequential on off test

--- QuadActuatorStandupDesk/Class1.cs
+++ QuadActuatorStandupDesk/Class1.cs
@@ -10,16 +10,44 @@ namespace QuadActuatorStandupDesk
         {
             var pigpiodIf = new PigpiodIf();
             pigpiodIf.pigpio_start("192.168.1.10", "8888");
-            const int GPIO = 5;
+
+            var delay = 100;
 
             while (true)
             {
-                pigpiodIf.gpio_write(GPIO, PigpiodIf.PI_HIGH);
-                Thread.Sleep(500);
-                pigpiodIf.gpio_write(GPIO, PigpiodIf.PI_LOW);
-                Thread.Sleep(500);
+                pigpiodIf.gpio_write(26, PigpiodIf.PI_HIGH);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(13, PigpiodIf.PI_HIGH);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(21, PigpiodIf.PI_HIGH);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(27, PigpiodIf.PI_HIGH);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(22, PigpiodIf.PI_HIGH);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(19, PigpiodIf.PI_HIGH);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(20, PigpiodIf.PI_HIGH);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(12, PigpiodIf.PI_HIGH);
+                Thread.Sleep(delay);               
+                pigpiodIf.gpio_write(26, PigpiodIf.PI_LOW);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(13, PigpiodIf.PI_LOW);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(21, PigpiodIf.PI_LOW);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(27, PigpiodIf.PI_LOW);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(22, PigpiodIf.PI_LOW);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(19, PigpiodIf.PI_LOW);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(20, PigpiodIf.PI_LOW);
+                Thread.Sleep(delay);
+                pigpiodIf.gpio_write(12, PigpiodIf.PI_LOW);
+                Thread.Sleep(delay);
             }
-
         }
     }
 }
